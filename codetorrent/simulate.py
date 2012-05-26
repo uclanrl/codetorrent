ba718d351ff1d05bdad1e24f20b5000cd1bcbf4e
Josh Joy <jjoy@cs.ucla.edu>
2012-05-25 19:03:23 -0700

    pass in argument of location of codetorrent

--- codetorrent/simulate.py
+++ codetorrent/simulate.py
@@ -1,4 +1,12 @@
+import sys
+
 from core import pycore
+
+if sys.argv[1]:
+	ct = sys.argv[1] + "/ct"
+else:
+	ct = "/home/josh/installs/codetorrent/codetorrent/codetorrent/ct"
+
 
 session = pycore.Session(persistent=True)
 node1 = session.addobj(cls=pycore.nodes.CoreNode, name="n1")
@@ -6,8 +14,6 @@ node2 = session.addobj(cls=pycore.nodes.CoreNode, name="n2")
 hub1 = session.addobj(cls=pycore.nodes.HubNode, name="hub1")
 node1.newnetif(hub1, ["10.0.0.1/24"])
 node2.newnetif(hub1, ["10.0.0.2/24"])
-
-ct = "/home/josh/installs/codetorrent/codetorrent/codetorrent/ct"
 
 node1.cmd([ct,"0","/home/josh/installs/codetorrent/codetorrent/codetorrent/test.txt","64","64","4","-v","10.0.0.255"],wait=False)
 node2.cmd([ct,"1","5","-v"],wait=True)
