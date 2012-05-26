a501aa8f791c2ffb2a74deb41ca6cd6a395fc52f
Josh Joy <jjoy@cs.ucla.edu>
2012-05-25 19:07:07 -0700

    adding run script pass in filename to simulate.py

---
+++ codetorrent/run.sh
@@ -0,0 +1,1 @@
+sudo python simulate.py /home/josh/installs/codetorrent/codetorrent/codetorrent/test.txt /home/josh/installs/codetorrent/codetorrent/codetorrent
--- codetorrent/simulate.py
+++ codetorrent/simulate.py
@@ -2,11 +2,14 @@ import sys
 
 from core import pycore
 
-if sys.argv[1]:
-	ct = sys.argv[1] + "/ct"
+file = sys.argv[1]
+
+if sys.argv[2]:
+	ct = sys.argv[2] + "/ct"
 else:
 	ct = "/home/josh/installs/codetorrent/codetorrent/codetorrent/ct"
 
+print file,ct
 
 session = pycore.Session(persistent=True)
 node1 = session.addobj(cls=pycore.nodes.CoreNode, name="n1")
@@ -15,7 +18,7 @@ hub1 = session.addobj(cls=pycore.nodes.HubNode, name="hub1")
 node1.newnetif(hub1, ["10.0.0.1/24"])
 node2.newnetif(hub1, ["10.0.0.2/24"])
 
-node1.cmd([ct,"0","/home/josh/installs/codetorrent/codetorrent/codetorrent/test.txt","64","64","4","-v","10.0.0.255"],wait=False)
+node1.cmd([ct,"0",file,"64","64","4","-v","10.0.0.255"],wait=False)
 node2.cmd([ct,"1","5","-v"],wait=True)
 session.shutdown()
 
